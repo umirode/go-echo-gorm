6add4258edef90c148f2b96597d50e2ca4c3ba33
makism.pavlov <Ghv%4jkl)7>
2018-12-25 16:07:11 +0300

    refactoring, rewrite to DDD

--- Http/Controller/BirthdayController.go
+++ Http/Controller/BirthdayController.go
@@ -3,6 +3,8 @@ package Controller
 import (
 	"github.com/labstack/echo"
 	"github.com/umirode/go-rest/Http/Error"
+	"github.com/umirode/go-rest/src/Application/Hydrator"
+	"github.com/umirode/go-rest/src/Common"
 	"github.com/umirode/go-rest/src/Domain/Service"
 	"github.com/umirode/go-rest/src/Domain/Service/DTO"
 	"net/http"
@@ -12,12 +14,14 @@ import (
 type BirthdayController struct {
 	BaseController
 
-	BirthdayService Service.IBirthdayService
+	BirthdayService  Service.IBirthdayService
+	BirthdayHydrator Common.IHydrator
 }
 
 func NewBirthdayController(birthdayService Service.IBirthdayService, userService Service.IUserService) *BirthdayController {
 	controller := &BirthdayController{
-		BirthdayService: birthdayService,
+		BirthdayService:  birthdayService,
+		BirthdayHydrator: &Hydrator.BirthdayHydrator{},
 	}
 
 	controller.UserService = userService
@@ -36,7 +40,13 @@ func (c *BirthdayController) Index(context echo.Context) error {
 		return err
 	}
 
-	return context.JSON(http.StatusOK, birthdays)
+	birthdaysMapArray := make([]map[string]interface{}, 0)
+	for _, holiday := range birthdays {
+		holidayMap, _ := c.BirthdayHydrator.Extract(holiday)
+		birthdaysMapArray = append(birthdaysMapArray, holidayMap)
+	}
+
+	return context.JSON(http.StatusOK, birthdaysMapArray)
 }
 
 func (c *BirthdayController) Create(context echo.Context) error {
@@ -67,7 +77,12 @@ func (c *BirthdayController) Create(context echo.Context) error {
 		return err
 	}
 
-	return c.BirthdayService.Create(birthdayDTO, user)
+	err = c.BirthdayService.Create(birthdayDTO, user)
+	if err != nil {
+		return err
+	}
+
+	return context.JSON(http.StatusOK, nil)
 }
 
 func (c *BirthdayController) Update(context echo.Context) error {
@@ -108,7 +123,12 @@ func (c *BirthdayController) Update(context echo.Context) error {
 		return err
 	}
 
-	return c.BirthdayService.Update(birthday, birthdayDTO, user)
+	err = c.BirthdayService.Update(birthday, birthdayDTO, user)
+	if err != nil {
+		return err
+	}
+
+	return context.JSON(http.StatusOK, nil)
 }
 
 func (c *BirthdayController) Delete(context echo.Context) error {
@@ -127,5 +147,10 @@ func (c *BirthdayController) Delete(context echo.Context) error {
 		return err
 	}
 
-	return c.BirthdayService.Delete(birthday, user)
+	err = c.BirthdayService.Delete(birthday, user)
+	if err != nil {
+		return err
+	}
+
+	return context.JSON(http.StatusOK, nil)
 }
---
+++ src/Application/Hydrator/BirthdayHydrator.go
@@ -0,0 +1,26 @@
+package Hydrator
+
+import (
+	"github.com/umirode/go-rest/src/Domain/Model/Entity"
+)
+
+type BirthdayHydrator struct {
+}
+
+func (*BirthdayHydrator) Create(data map[string]interface{}) (interface{}, error) {
+	panic("implement me")
+}
+
+func (*BirthdayHydrator) Hydrate(data map[string]interface{}, object interface{}) (interface{}, error) {
+	panic("implement me")
+}
+
+func (*BirthdayHydrator) Extract(object interface{}) (map[string]interface{}, error) {
+	birthday := object.(*Entity.Birthday)
+
+	return map[string]interface{}{
+		"id":   birthday.ID,
+		"date": birthday.Date,
+		"name": birthday.Name,
+	}, nil
+}
